79ad15b9722f49749496b2989e177443a370cf64
PrathyushNair <prathyu360@gmail.com>
2022-07-19 21:55:17 +0530

    testing for conflict

--- testproj/src/Components/shivajis compo/Shivaji.jsx
+++ testproj/src/Components/shivajis compo/Shivaji.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Shivaji = () => {
-  return <div>Shivaji</div>;
+  return <div>Shivaji
+
+    <h2>Hello from jijabai</h2>
+  </div>;
 };
 
 export default Shivaji;
